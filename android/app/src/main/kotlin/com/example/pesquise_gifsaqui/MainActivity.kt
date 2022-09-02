8aa18d0a7ed4a68b36f76e616af882437a1506b6
lucaspaludo <lucaspaludo2018@gmail.com>
2022-09-02 19:49:29 -0300

    Commit!

---
+++ android/app/src/main/kotlin/com/example/pesquise_gifsaqui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pesquise_gifsaqui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:pesquise_gifsaqui/ui/HomePage.dart';
+import 'package:pesquise_gifsaqui/ui/gif_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    theme: ThemeData(hintColor: Colors.white),
+    ));
+}
+
